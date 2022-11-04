b0f952e951906b6bd1e265865002407267c28317
manuelmatosa <jchu2001@gmail.com>
2022-11-04 20:35:12 +0100

    2

---
+++ app/src/main/java/com/example/supermercado/Prueba.kt
@@ -0,0 +1,22 @@
+package com.example.supermercado
+
+class Prueba {
+    private void Prueba > {
+
+
+
+                val username
+                val food = s1[4]
+
+                try {
+
+                } catch (e: Exception) {
+                    println("Failed:");
+                    throw e
+                }
+            }
+        }
+
+        return projectsMap
+    }
+}
